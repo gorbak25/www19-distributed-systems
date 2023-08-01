cbffa6af877b180236ce49882c54d0e14a5905b2
Grzegorz Uriasz <gorbak25@gmail.com>
2023-08-01 09:35:20 +0000

    Solutions for ExampleGenServer

--- day2/lib/exampleGenServer.ex
+++ day2/lib/exampleGenServer.ex
@@ -14,17 +14,32 @@ defmodule ExampleGenServer do
 
   @impl true
   def init(stack) do
-    {:ok, stack}
+    {:ok, {{stack, 0}, []}}
   end
 
   @impl true
-  def handle_call(:pop, _from, [head | tail]) do
-    {:reply, head, tail}
+  def handle_call(:pop, _from, {{[head | tail], l}, s}) do
+    {:reply, {:ok, head}, {{tail, l-1}, s}}
+  end
+  def handle_call(:pop, _from, {{[], 0}, _} = state) do
+    {:reply, {:err, "stack empty"}, state}
+  end
+  def handle_call(:len, _from, {{_, l}, _} = state) do
+    {:reply, l, state}
+  end
+  def handle_call(:restore, _from, {_, [s1 | sr]}) do
+    {:reply, :ok, {s1, sr}}
+  end
+  def handle_call(:restore, _from, {_, []} = state) do
+    {:reply, {:err, "no snapshots"}, state}
   end
 
   @impl true
-  def handle_cast({:push, element}, state) do
-    {:noreply, [element | state]}
+  def handle_cast({:push, element}, {{state, l}, s}) do
+    {:noreply, {{[element | state], l+1}, s}}
+  end
+  def handle_cast(:snapshot, {s1, sr}) do
+    {:noreply, {s1, [s1 | sr]}}
   end
 
   def pop() do
@@ -32,5 +47,15 @@ defmodule ExampleGenServer do
   end
   def push(x) do
     GenServer.cast(@me, {:push, x})
+  end
+  def len() do
+    GenServer.call(@me, :len)
+  end
+
+  def snapshot() do
+    GenServer.cast(@me, :snapshot)
+  end
+  def restore() do
+    GenServer.call(@me, :restore)
   end
 end
